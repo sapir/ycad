777881b1fe247cde849d19bc128244d02304fed8
Y. Sapir <yasapir@gmail.com>
2013-08-16 14:36:06 +0300

    Add cylinder() builtin.

--- runtime.py
+++ runtime.py
@@ -7,8 +7,20 @@ def cube(ctx, s):
     assert isinstance(s, float)
     return 'cube(size={0});'.format(s)
 
+def cylinder(ctx, h, d, d1=None, d2=None):
+    assert isinstance(h, float)
+    assert d is None or isinstance(d, float)
+    assert d1 is None or isinstance(d1, float)
+    assert d2 is None or isinstance(d2, float)
+    assert (d is not None) ^ (d1 is not None and d2 is not None)
+    
+    if d is not None:
+        return 'cylinder(h={0}, r={1});'.format(h, d/2.)
+    elif d1 is not None and d2 is not None:
+        return 'cylinder(h={0}, r1={1}, r2={2});'.format(h, d1/2., d2/2.)
+
 builtins = dict((f.func_name, f)
-    for f in [cube])
+    for f in [cube, cylinder])
 
 
 class Context:
