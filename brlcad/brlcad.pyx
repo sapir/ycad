fea63dfa6e7e40d0fbdc1d8b9de21e79a66840d4
Y. Sapir <yasapir@gmail.com>
2013-08-20 01:15:17 +0300

    Add beginning of a Cython wrapper for BRL-CAD.

---
+++ brlcad/brlcad.pyx
@@ -0,0 +1,54 @@
+cimport c_brlcad
+
+import numpy as np
+cimport numpy as np
+
+
+class BrlcadError(StandardError):
+    pass
+
+cdef _check_res(int res):
+    if res != 0:
+        raise BrlcadError()
+
+cdef np.ndarray[c_brlcad.fastf_t, ndim=1] _as_vec(lst):
+    return np.array(lst, dtype='double')
+
+
+cdef class WDB:
+    cdef c_brlcad.rt_wdb *ptr
+
+    def mk_id(self, bytes name, bytes unit_name=None):
+        cdef int res
+
+        if unit_name is None:
+            res = c_brlcad.mk_id(self.ptr, name)
+        else:
+            res = c_brlcad.mk_id_units(self.ptr, name, unit_name)
+
+        _check_res(res)
+    
+    def mk_rpp(self, bytes name, min_p, max_p):
+        res = c_brlcad.mk_rpp(self.ptr, name,
+            <c_brlcad.point_t> _as_vec(min_p).data,
+            <c_brlcad.point_t> _as_vec(max_p).data)
+
+        _check_res(res)
+
+    def mk_sph(self, bytes name, center, radius):
+        res = c_brlcad.mk_sph(self.ptr, name,
+            <c_brlcad.point_t> _as_vec(center).data, radius)
+        
+        _check_res(res)
+
+    cpdef close(self):
+        c_brlcad.wdb_close(self.ptr)
+
+def wdb_fopen(filename):
+    db = WDB()
+    
+    db.ptr = c_brlcad.wdb_fopen(filename)
+    if db.ptr is NULL:
+        raise BrlcadError()
+
+    return db
---
+++ brlcad/c_brlcad.pxd
@@ -0,0 +1,26 @@
+cdef extern from "common.h":
+    pass
+
+cdef extern from "vmath.h":
+    ctypedef double fastf_t     # actually from bu.h, but vmath.h includes it
+    ctypedef fastf_t point_t[3]
+
+cdef extern from "raytrace.h":
+    struct rt_wdb:
+        pass
+
+    struct db_i:
+        pass
+
+cdef extern from "rtgeom.h":
+    pass
+
+cdef extern from "wdb.h":
+    rt_wdb *wdb_fopen(char *filename)
+    void wdb_close(rt_wdb *db)
+
+    int mk_id(rt_wdb *db, char *name)
+    int mk_id_units(rt_wdb *db, char *name, char *units)
+
+    int mk_rpp(rt_wdb *db, char *name, point_t min_p, point_t max_p)
+    int mk_sph(rt_wdb *db, char *name, point_t center, fastf_t radius)
