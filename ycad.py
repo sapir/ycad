bcf1b6868c782f692c894f1cdb76d9e3f8ef131e
Y. Sapir <yasapir@gmail.com>
2013-10-10 23:58:45 +0300

    Show total time even on error.

--- ycad.py
+++ ycad.py
@@ -20,24 +20,26 @@ if __name__ == '__main__':
     
     startTime = time.time()
 
-    print('Initializing...', file=sys.stderr)
-    import grammar
-    import runtime
-    timeAfterInit = time.time()
-    print('Initialization time: {0:.2f}s'.format(timeAfterInit - startTime))
-
-    print('Parsing...', file=sys.stderr)
     try:
-        parsed = grammar.parseFile(args.filename)
-    finally:
-        timeAfterParsing = time.time()
-        print('Parse time: {0:.2f}s'.format(timeAfterParsing - timeAfterInit))
+        print('Initializing...', file=sys.stderr)
+        import grammar
+        import runtime
+        timeAfterInit = time.time()
+        print('Initialization time: {0:.2f}s'.format(timeAfterInit - startTime))
 
-    print('Running...', file=sys.stderr)
-    try:
-        runtime.run(os.path.abspath(args.filename), parsed, args.output)
+        print('Parsing...', file=sys.stderr)
+        try:
+            parsed = grammar.parseFile(args.filename)
+        finally:
+            timeAfterParsing = time.time()
+            print('Parse time: {0:.2f}s'.format(timeAfterParsing - timeAfterInit))
+
+        print('Running...', file=sys.stderr)
+        try:
+            runtime.run(os.path.abspath(args.filename), parsed, args.output)
+        finally:
+            timeAfterRunning = time.time()
+            print('Execution time: {0:.2f}s'.format(timeAfterRunning - timeAfterParsing))
     finally:
         endTime = time.time()
-
-        print('Execution time: {0:.2f}s'.format(endTime - timeAfterParsing))
         print('Total time: {0:.2f}s'.format(endTime - startTime))
