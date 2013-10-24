72aa05cf6578f9f2dc741e165eed642f5702283e
Y. Sapir <yasapir@gmail.com>
2013-10-25 02:41:39 +0300

    Add support for bool ops in cython wrapper, and simplify code a bit.

--- _ycad.pyx
+++ _ycad.pyx
@@ -12,20 +12,53 @@ cdef extern from "TopoDS_Shape.hxx":
     cdef cppclass TopoDS_Shape:
         pass
 
-cdef class Shape:
-    cdef TopoDS_Shape obj
+cdef extern from "BRepBuilderAPI_MakeShape.hxx":
+    cdef cppclass BRepBuilderAPI_MakeShape:
+        const TopoDS_Shape &Shape()
 
 
 
 cdef extern from "BRepPrimAPI_MakeBox.hxx":
-    cdef cppclass BRepPrimAPI_MakeBox:
+    cdef cppclass BRepPrimAPI_MakeBox(BRepBuilderAPI_MakeShape):
         BRepPrimAPI_MakeBox(Standard_Real, Standard_Real, Standard_Real)
-        const TopoDS_Shape &Shape()
 
 def box(float x, float y, float z):
-    s = Shape()
-    s.obj = BRepPrimAPI_MakeBox(x, y, z).Shape()
-    return s
+    return Shape().setFromMaker(BRepPrimAPI_MakeBox(x, y, z))
+
+
+cdef extern from "BRepAlgoAPI_Fuse.hxx":
+    cdef cppclass BRepAlgoAPI_Fuse(BRepBuilderAPI_MakeShape):
+        BRepAlgoAPI_Fuse(TopoDS_Shape, TopoDS_Shape)
+
+cdef extern from "BRepAlgoAPI_Cut.hxx":
+    cdef cppclass BRepAlgoAPI_Cut(BRepBuilderAPI_MakeShape):
+        BRepAlgoAPI_Cut(TopoDS_Shape, TopoDS_Shape)
+
+cdef extern from "BRepAlgoAPI_Common.hxx":
+    cdef cppclass BRepAlgoAPI_Common(BRepBuilderAPI_MakeShape):
+        BRepAlgoAPI_Common(TopoDS_Shape, TopoDS_Shape)
+
+
+
+cdef class Shape:
+    cdef TopoDS_Shape obj
+
+    cdef set_(self, TopoDS_Shape obj):
+        self.obj = obj
+        return self
+
+    cdef setFromMaker(self, const BRepBuilderAPI_MakeShape &maker):
+        self.obj = maker.Shape()
+        return self
+
+    def __add__(Shape self, Shape b):
+        return Shape().setFromMaker(BRepAlgoAPI_Fuse(self.obj, b.obj))
+    
+    def __sub__(Shape self, Shape b):
+        return Shape().setFromMaker(BRepAlgoAPI_Cut(self.obj, b.obj))
+
+    def __mul__(Shape self, Shape b):
+        return Shape().setFromMaker(BRepAlgoAPI_Common(self.obj, b.obj))
 
 
 cdef extern from "_ycad_helpers.h":
